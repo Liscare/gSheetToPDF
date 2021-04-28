0abcef24176802dfc2b91486e216ef8418167e41
Liscare <liscare2@protonmail.com>
2021-04-28 21:36:34 +0200

    Cleaning code (rename variables, functions etc)

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
-var saveToRootFolder = true
-var email = "liscare2@protonmail.com";
-var choices = ["?", "?"];
+const gSpreadsheet = SpreadsheetApp.openById('ID of your GSheet')
+const gPdfSheet = gSpreadsheet.getSheetByName('Name of a sheet')
+
+var gSaveToRootFolder = true
+var gChoices = ["?", "?"];
+var gEmail = "liscare2@protonmail.com";
 
 /**
- * Submit form handler
- * Generate a PDF file from a gsheet according to the responses (email address and data choice)
+ * Submit form handler.
+ * Generate a PDF file from a gsheet according to the responses (email address and data choice).
  */
 function onSubmitForm(event) {
     // Fetch form response
     let responseItems = event.response.getItemResponses()
     if (responseItems) {
-        choices = responseItems[1].getResponse().split("-") // Can be Python - argparse
-        email = responseItems[0].getResponse() // Email address
+        gChoices = responseItems[1].getResponse().split("-") // Can be Python - argparse
+        gEmail = responseItems[0].getResponse() // Email address
     }
-    // Insert choices into a google sheet to change data in the sheet (and then generate the PDF)
-    let sheet = SpreadsheetApp.openById('ID of your GSheet');
-    let arr = [[choices[0], choices[1]]]
-    // Cell R5 is for choices[0]
-    // Cell S5 is for choices[1]
+    // Insert gChoices into a google sheet to change data in the sheet (and then generate the PDF)
+    let arr = [[gChoices[0], gChoices[1]]]
+    // Cell R5 is for gChoices[0]
+    // Cell S5 is for gChoices[1]
     // Other cells are linked to those 2 cells
-    sheet.getSheetByName('Name of a sheet').getRange('R5:S5').setValues(arr)
-    SpreadsheetApp.flush() //
-    exportCurrentSheetAsPDF()
+    gPdfSheet.getRange('R5:S5').setValues(arr)
+    SpreadsheetApp.flush()
+    exportSelectedSheetAsPdf()
 }
 
+function exportSelectedSheetAsPdf() {
+    let blob = getAsBlob(gSpreadsheet.getUrl(), gPdfSheet)
+    sendEmailWithFile(blob, gPdfSheet.getName(), gSpreadsheet)
+}
 
-function _exportBlob(blob, fileName, spreadsheet) {
+function sendEmailWithFile(blob, fileName, spreadsheet) {
     blob = blob.setName(fileName)
-    var folder = saveToRootFolder ? DriveApp : DriveApp.getFileById(spreadsheet.getId()).getParents().next()
-    var pdfFile = folder.createFile(blob)
+    let folder = gSaveToRootFolder ? DriveApp : DriveApp.getFileById(spreadsheet.getId()).getParents().next()
+    let pdfFile = folder.createFile(blob)
 
     MailApp.sendEmail({
-        to: email,
+        to: gEmail,
         subject: "A subject",
         htmlBody: "Hi, I send you a PDf file from a Google Sheet.",
         attachments: [pdfFile.getAs(MimeType.PDF)]
     });
-
 }
 
-function exportAsPDF() {
-    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-    var blob = _getAsBlob(spreadsheet.getUrl())
-    _exportBlob(blob, spreadsheet.getName(), spreadsheet)
-}
-
-function _getAsBlob(url, sheet, range) {
-    var rangeParam = ''
-    var sheetParam = ''
+function getAsBlob(url, sheet, range) {
+    let rangeParam = ''
+    let sheetParam = ''
     if (range) {
         rangeParam =
             '&r1=' + (range.getRow() - 1)
@@ -61,7 +60,7 @@ function _getAsBlob(url, sheet, range) {
     // A credit to https://gist.github.com/Spencer-Easton/78f9867a691e549c9c70
     // these parameters are reverse-engineered (not officially documented by Google)
     // they may break overtime.
-    var exportUrl = url.replace(/\/edit.*$/, '')
+    let exportUrl = url.replace(/\/edit.*$/, '')
         + '/export?exportFormat=pdf&format=pdf'
         + '&size=LETTER'
         + '&portrait=true'
@@ -77,8 +76,8 @@ function _getAsBlob(url, sheet, range) {
         + sheetParam
         + rangeParam
 
-    var response
-    var i = 0
+    let response
+    let i = 0
     for (; i < 5; i += 1) {
         response = UrlFetchApp.fetch(exportUrl, {
             muteHttpExceptions: true,
@@ -99,13 +98,4 @@ function _getAsBlob(url, sheet, range) {
     }
 
     return response.getBlob()
-}
-
-
-function exportCurrentSheetAsPDF() {
-    var spreadsheet = SpreadsheetApp.openById('ID of your GSheet')
-    var currentSheet = spreadsheet.getSheetByName('Name of a sheet')
-
-    let blob = _getAsBlob(spreadsheet.getUrl(), currentSheet)
-    _exportBlob(blob, currentSheet.getName(), spreadsheet)
 }
